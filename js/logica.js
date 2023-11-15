033e811965f7289fda57788916411c1127c0cb4b
CIANDT\ademirj <ademirj@ciandt.com>
2023-11-14 22:39:10 -0300

    Modificando logica.js

--- js/logica.js
+++ js/logica.js
@@ -1,75 +1,56 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("cadastro-form");
-    const enviarButton = document.getElementById("enviar-button");
+const { google } = require('googleapis');
+const fs = require('fs');
 
-    enviarButton.addEventListener("click", function () {
-        const tipoContribuicao = document.getElementById("tipo-contribuicao").value;
-        const valor = document.getElementById("valor").value;
-        const data = document.getElementById("data").value;
-        const nomeContribuinte = document.getElementById("nome-contribuinte").value;
+// Carregue o arquivo JSON de credenciais
+const CREDENCIAIS_PATH = 'client_secret_929529148669-4ocjeeso322seofkkcsrutrkeibacbs1.apps.googleusercontent.com.json';
+const CREDENCIAIS = JSON.parse(fs.readFileSync(CREDENCIAIS_PATH));
 
-        const confirmacao = confirm("Por favor, confirme as informações:\n\n" +
-            `Tipo de Contribuição: ${tipoContribuicao}\n` +
-            `Valor: R$ ${valor}\n` +
-            `Data: ${data}\n` +
-            `Nome do Contribuinte: ${nomeContribuinte}`);
+// Configure as informações de autenticação
+const CLIENT_ID = CREDENCIAIS.client_id;
+const CLIENT_SECRET = CREDENCIAIS.client_secret;
+const REDIRECT_URI = CREDENCIAIS.redirect_uris[0];
 
-        if (confirmacao) {
-            const comprovante = `Tipo de Contribuição: ${tipoContribuicao}\n` +
-                `Valor: R$ ${valor}\n` +
-                `Data: ${data}\n` +
-                `Nome do Contribuinte: ${nomeContribuinte}`;
+// Crie uma instância do cliente OAuth 2.0
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
-            const comprovanteParaDownload = encodeURIComponent(comprovante);
-            const downloadLink = document.createElement('a');
-            downloadLink.href = `data:text/plain;charset=utf-8,${comprovanteParaDownload}`;
-            downloadLink.download = 'comprovante.txt';
-            downloadLink.style.display = 'none';
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-            document.body.removeChild(downloadLink);
+// Defina as permissões necessárias para a API do Google Sheets
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
-            const whatsappLink = `https://wa.me/?text=${encodeURIComponent(comprovante)}`;
-            window.open(whatsappLink, '_blank');
+// Autentique o cliente usando as credenciais
+oAuth2Client.setCredentials(CREDENCIAIS);
 
-            // Limpar os campos após o envio
-            document.getElementById("tipo-contribuicao").value = "";
-            document.getElementById("valor").value = "";
-            document.getElementById("data").value = "";
-            document.getElementById("nome-contribuinte").value = "";
-
-            // Adicionar a requisição à API do Google Sheets aqui
-            adicionarLinha(tipoContribuicao, valor, data, nomeContribuinte);
-        }
-    });
-});
+// Agora você pode usar o cliente OAuth 2.0 autenticado para fazer chamadas à API do Google Sheets.
+adicionarLinha(oAuth2Client);
 
 // Função para adicionar uma linha à planilha do Google Sheets
-function adicionarLinha(tipoContribuicao, valor, data, nomeContribuinte) {
-    const API_KEY = 'AIzaSyD3xo7xTfTidvYtZ81maokmZbWFoLXEEEw'; // Substitua pela sua chave de API do Google Sheets
-    const SPREADSHEET_ID = '15bxULWG5MQjFhLttVDgpVtn4azaY4zf2lzcc2ekm7ug'; // Substitua pelo ID da sua planilha do Google Sheets
+function adicionarLinha(auth) {
+  const sheets = google.sheets({ version: 'v4', auth });
 
-    // Crie uma matriz de valores a serem adicionados
-    const valores = [
-        [tipoContribuicao, valor, data, nomeContribuinte],
-    ];
+  const SPREADSHEET_ID = '15bxULWG5MQjFhLttVDgpVtn4azaY4zf2lzcc2ekm7ug'; // Substitua pelo ID da sua planilha
+  const RANGE = 'A1'; // Substitua pela faixa onde deseja adicionar os valores
 
-    // Exemplo de como adicionar os valores à planilha usando fetch
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/A1:append?valueInputOption=RAW`, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            values: valores,
-        }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Linha adicionada à planilha:', data);
-    })
-    .catch(error => {
-        console.error('Erro ao adicionar linha à planilha:', error);
-    });
+  const tipoContribuicao = 'Dízimo'; // Substitua pelo valor desejado
+  const valor = 100.00; // Substitua pelo valor desejado
+  const data = '2023-11-15'; // Substitua pela data desejada
+  const nomeContribuinte = 'John Doe'; // Substitua pelo nome desejado
+
+  const valores = [
+    [tipoContribuicao, valor, data, nomeContribuinte],
+  ];
+
+  sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: RANGE,
+    valueInputOption: 'RAW',
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      values: valores,
+    },
+  }, (err, res) => {
+    if (err) {
+      console.error('Erro ao adicionar linha à planilha:', err);
+    } else {
+      console.log('Linha adicionada à planilha:', res.data);
+    }
+  });
 }
